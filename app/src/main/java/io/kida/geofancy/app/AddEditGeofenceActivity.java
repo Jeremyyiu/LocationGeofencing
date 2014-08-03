d50ad1c3cee3761a55ec416727a5822e55817839
Marcus Kida <marcus@kida.io>
2014-08-03 13:56:53 +1000

    Implements basic Add/Edit Geofence UI, LocationManager and Reverse Geocoder

--- app/src/main/java/io/kida/geofancy/app/AddEditGeofenceActivity.java
+++ app/src/main/java/io/kida/geofancy/app/AddEditGeofenceActivity.java
@@ -1,18 +1,28 @@
 package io.kida.geofancy.app;
 
 import android.app.FragmentManager;
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
@@ -28,59 +38,52 @@ import com.schuetz.mapareas.MapAreaManager.CircleManagerListener;
 import com.schuetz.mapareas.MapAreaMeasure;
 import com.schuetz.mapareas.MapAreaWrapper;
 
-public class AddEditGeofenceActivity extends FragmentActivity implements LocationListener {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class AddEditGeofenceActivity extends FragmentActivity {
+    private static final int UPDATE_ADDRESS = 1;
 
     private MapFragment mMapFragment = null;
     private GoogleMap mMap = null;
-    private LocationManager mLocationManager = null;
+    private MyLocation mMyLocation = null;
     private SeekBar mRadiusSlider = null;
 
     private MapAreaManager mCircleManager = null;
     private MapAreaWrapper mCircle = null;
 
-    @Override
-    public void onProviderEnabled(String provider) {
+    private Handler mGeocoderHandler = null;
 
-    }
+    // UI
+    private EditText mCustomId = null;
 
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        if (mMap != null) {
-            zoomToLocation(location);
+    MyLocation.LocationResult locationResult = new MyLocation.LocationResult(){
+        @Override
+        public void gotLocation(Location location){
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            if (mMap != null) {
+                zoomToLocation(location);
+            }
+            setCircleToLocation(location);
+            doReverseGeocoding(location);
         }
-        mLocationManager.removeUpdates(this);
-
-        // Set Circle
-        if (mMap != null && mCircleManager != null && mCircle == null) {
-            LatLng position = new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
-            mCircle = new MapAreaWrapper(mMap, position, 100, 5.0f, 0xffff0000, 0x33ff0000, 1, 1000);
-            mCircleManager.add(mCircle);
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        if (status == LocationProvider.AVAILABLE) {
-            mLocationManager.requestLocationUpdates(mLocationManager.getBestProvider(new Criteria(), true), 5000, 0, this);
-        }
-    }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_geofence);
 
+        mCustomId = (EditText)findViewById(R.id.customLocationId);
+
         mRadiusSlider = (SeekBar)findViewById(R.id.radius_slider);
         mRadiusSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                mCircle.setRadius(progress * 10);
+                if (mCircle != null) {
+                    mCircle.setRadius(progress * 10);
+                }
             }
 
             @Override
@@ -102,18 +105,29 @@ public class AddEditGeofenceActivity extends FragmentActivity implements Locatio
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(false);
 //        mMap.getMap().setMapType(0);
+
         if(mMap.getMyLocation() != null) {
             Location location = mMap.getMyLocation();
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 16));
         }
 
-        mLocationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        String provider = mLocationManager.getBestProvider(criteria, true);
-        Location location = mLocationManager.getLastKnownLocation(provider);
-        mLocationManager.requestLocationUpdates(provider, 20000, 0, this);
+        mMyLocation = new MyLocation();
+        mMyLocation.getLocation(this, locationResult);
 
         setupCircleManager();
+
+        // Reverse Geocoder Handler
+        mGeocoderHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case UPDATE_ADDRESS: {
+                        Button button = (Button) findViewById(R.id.address_button);
+                        button.setText((String) msg.obj);
+                        break;
+                    }
+                }
+            }
+        };
     }
 
 
@@ -130,9 +144,9 @@ public class AddEditGeofenceActivity extends FragmentActivity implements Locatio
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -140,6 +154,16 @@ public class AddEditGeofenceActivity extends FragmentActivity implements Locatio
     private void zoomToLocation(Location location){
         LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, 16));
+    }
+
+    // Setup Circle
+    private void setCircleToLocation(Location location) {
+        // Set Circle
+        if (mMap != null && mCircleManager != null && mCircle == null) {
+            LatLng position = new LatLng(location.getLatitude(), location.getLongitude());
+            mCircle = new MapAreaWrapper(mMap, position, 100, 5.0f, 0xffff0000, 0x33ff0000, 1, 1000);
+            mCircleManager.add(mCircle);
+        }
     }
 
     // Radius Circle
@@ -191,5 +215,47 @@ public class AddEditGeofenceActivity extends FragmentActivity implements Locatio
 
             }
         });
+    }
+
+    // Reverse Geocoder
+    private void doReverseGeocoding(Location location) {
+        // Since the geocoding API is synchronous and may take a while.  You don't want to lock
+        // up the UI thread.  Invoking reverse geocoding in an AsyncTask.
+        (new ReverseGeocodingTask(this)).execute(new Location[] {location});
+    }
+
+    private class ReverseGeocodingTask extends AsyncTask<Location, Void, Void> {
+        Context mContext;
+
+        public ReverseGeocodingTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Location... params) {
+            Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+
+            Location loc = params[0];
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Update address field with the exception.
+                Message.obtain(mGeocoderHandler, UPDATE_ADDRESS, e.toString()).sendToTarget();
+            }
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                // Format the first line of address (if available), city, and country name.
+                String addressText = String.format("%s, %s, %s",
+                        address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
+                        address.getLocality(),
+                        address.getCountryName());
+                // Update address field on UI.
+                Message.obtain(mGeocoderHandler, UPDATE_ADDRESS, addressText).sendToTarget();
+            }
+            return null;
+        }
     }
 }
