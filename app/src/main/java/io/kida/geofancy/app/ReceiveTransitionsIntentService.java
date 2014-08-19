41e54a8479dcc58db80800974e7a5cce0f198d34
Marcus Kida <marcus@kida.io>
2014-08-19 22:32:18 +1000

    Get Geofence from SQLite on Event and show name in Notification

--- app/src/main/java/io/kida/geofancy/app/ReceiveTransitionsIntentService.java
+++ app/src/main/java/io/kida/geofancy/app/ReceiveTransitionsIntentService.java
@@ -3,7 +3,10 @@ package io.kida.geofancy.app;
 import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -49,6 +52,19 @@ public class ReceiveTransitionsIntentService extends IntentService {
 
     private void processGeofence(Geofence geofence, int transitionType) {
 
+        ContentResolver resolver = this.getContentResolver();
+        Cursor cursor = resolver.query(Uri.parse("content://" + getString(R.string.authority) + "/geofences"), null, "_id = ?", new String[]{ geofence.getRequestId() }, null);
+        if (cursor.getCount() == 0) {
+            return;
+        }
+        cursor.moveToFirst();
+
+        String locationName = cursor.getString(cursor.getColumnIndex("name"));
+        if (locationName.length() == 0) {
+            locationName = "Unknown Location";
+        }
+        String transition = transitionType == Geofence.GEOFENCE_TRANSITION_ENTER ? "Enter " : "Exit ";
+
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext());
 
         PendingIntent openActivityIntetnt = PendingIntent.getActivity(this, 0, new Intent(this, GeofencesActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
@@ -56,8 +72,9 @@ public class ReceiveTransitionsIntentService extends IntentService {
 
         notificationBuilder
                 .setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle("Geofence id: " + id)
-                .setContentText("Transition type: " + getTransitionTypeString(transitionType))
+//                .setContentTitle("Geofence id: " + id)
+                .setContentTitle(locationName)
+                .setContentText("Has been " + getTransitionTypeString(transitionType))
                 .setVibrate(new long[]{500, 500})
                 .setContentIntent(openActivityIntetnt)
                 .setAutoCancel(true);
@@ -71,13 +88,13 @@ public class ReceiveTransitionsIntentService extends IntentService {
     private String getTransitionTypeString(int transitionType) {
         switch (transitionType) {
             case Geofence.GEOFENCE_TRANSITION_ENTER:
-                return "enter";
+                return "entered";
             case Geofence.GEOFENCE_TRANSITION_EXIT:
-                return "exit";
+                return "left";
             case Geofence.GEOFENCE_TRANSITION_DWELL:
-                return "dwell";
+                return "dwelled";
             default:
-                return "unknown";
+                return "happened an unknown event.";
         }
     }
 
