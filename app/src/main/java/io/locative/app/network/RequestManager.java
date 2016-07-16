3c0c4520c0d9a21dd70841f99fad0c5492c6c0fe
Marcus Kida <marcus@kida.io>
2016-07-16 16:53:06 +1000

    add request form body to `RequestManager`

--- app/src/main/java/io/locative/app/network/RequestManager.java
+++ app/src/main/java/io/locative/app/network/RequestManager.java
@@ -2,19 +2,16 @@ package io.locative.app.network;
 
 import android.content.SharedPreferences;
 
-import java.io.IOException;
-
 import javax.inject.Inject;
 
 import io.locative.app.model.Geofences;
 import io.locative.app.utils.Constants;
 import io.locative.app.utils.Preferences;
 import okhttp3.Callback;
-import okhttp3.MediaType;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okio.BufferedSink;
 
 public class RequestManager {
 
@@ -24,17 +21,14 @@ public class RequestManager {
     private final OkHttpClient client = new OkHttpClient();
 
     public void perform (Geofences.Geofence geofence, int transition, Callback callback) {
-        final RequestBody body = new RequestBody() {
-            @Override
-            public MediaType contentType() {
-                return null;
-            }
-
-            @Override
-            public void writeTo(BufferedSink sink) throws IOException {
-
-            }
-        };
+        final RequestBody body = new FormBody.Builder()
+                .add("latitude", String.valueOf(geofence.latitude))
+                .add("longitude", String.valueOf(geofence.longitude))
+                .add("id", geofence.id)
+                .add("device", "")
+                .add("trigger", fromTransition(transition))
+                .add("timestamp", null)
+                .build();
         final Request request = new Request.Builder()
                 .url(mPreferences.getString(Preferences.HTTP_URL, ""))
                 .method(fromMethod(mPreferences.getInt(Preferences.HTTP_METHOD, 0)), body)
@@ -44,5 +38,9 @@ public class RequestManager {
 
     private String fromMethod (int method) {
         return method == Constants.HttpMethod.POST.ordinal() ? "POST" : "GET";
+    }
+
+    private String fromTransition (int transition) {
+        return transition == com.google.android.gms.location.Geofence.GEOFENCE_TRANSITION_ENTER ? "enter" : "exit";
     }
 }
